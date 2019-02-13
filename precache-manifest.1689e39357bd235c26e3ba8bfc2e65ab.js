53e1ef9ae9dfce8bc9669a7e5064d8580082bb44
Mkim <sp3dng9@gmail.com>
2019-02-13 01:02:53 -0800

    Updates

---
+++ precache-manifest.1689e39357bd235c26e3ba8bfc2e65ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "94d8cb3cbfef249f1751",
+    "url": "/clicky-game/static/js/main.94d8cb3c.chunk.js"
+  },
+  {
+    "revision": "bf42c74bd62676cbc049",
+    "url": "/clicky-game/static/js/2.bf42c74b.chunk.js"
+  },
+  {
+    "revision": "94d8cb3cbfef249f1751",
+    "url": "/clicky-game/static/css/main.0977f8ee.chunk.css"
+  },
+  {
+    "revision": "cf654e5357058b38cfcec79ac35cc5b1",
+    "url": "/clicky-game/index.html"
+  }
+];
